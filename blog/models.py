4ea0b6870d47e9675e53890ca4a5e72ebdd0120a
kelvinhovinho <kelvinadamba@hotmail.com>
2025-05-06 00:11:16 +0300

    Finished models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
